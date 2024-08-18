38a86c42e7a0544e4170e19109a0f495cd183db0
x <oskarmansen@proton.me>
2024-08-18 20:02:05 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,133 @@
-use sysinfo::System;
+use sysinfo::{System, SystemExt, ProcessExt};
 use windows::core::{s, PSTR};
 use windows::Win32::System::SystemInformation::{
     GetSystemInfo, GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO,
 };
-
 use windows::Win32::System::Registry::{
     RegCloseKey, RegOpenKeyExA, RegQueryInfoKeyA, HKEY, HKEY_LOCAL_MACHINE, KEY_READ,
 };
+use std::time::Duration;
+use std::thread;
 
 fn main() {
-    verify_usb();
-    verify_ram();
-    verify_cpu();
-    verify_processes();
+    println!("Yo, let's see if we're in some fake computer world or whatever...");
+    
+    let mut sus_points = 0;
+    
+    sus_points += check_usb_stuff();
+    sus_points += look_at_ram();
+    sus_points += count_cpu_thingies();
+    sus_points += see_whats_running();
+    
+    thread::sleep(Duration::from_secs(2));
+    println!("\nOk, moment of truth...");
+    thread::sleep(Duration::from_secs(1));
+    
+    if sus_points >= 2 {
+        println!("Bruh, this is mad sus! Probs a fake computer. Sus level: {}", sus_points);
+    } else {
+        println!("Looks legit, I guess. Sus level: {}", sus_points);
+    }
 }
 
-/*
-    Function that checks the CPU to determine the number of processors in the computer.
-*/
-fn verify_cpu() {
+fn count_cpu_thingies() -> u32 {
+    println!("Counting those CPU core thingies...");
     let mut info: SYSTEM_INFO = SYSTEM_INFO::default();
-
     unsafe {
         GetSystemInfo(&mut info);
     }
-
     if info.dwNumberOfProcessors < 2 {
-        println!("[*] Possibly a virtualised environment")
+        println!("LOL, only one core? What is this, a calculator?");
+        1
+    } else {
+        println!("Cool, {} cores. Not too shabby!", info.dwNumberOfProcessors);
+        0
     }
 }
 
-/*
- Function that checks the current physical memory in bytes and verifies if it is greater than or equal to two gigabytes.
-*/
-fn verify_ram() {
+fn look_at_ram() -> u32 {
+    println!("Checking out the RAM sitch...");
     let mut info: MEMORYSTATUSEX = MEMORYSTATUSEX::default();
     info.dwLength = std::mem::size_of::<MEMORYSTATUSEX>() as u32;
-
     unsafe {
-        let _ = GlobalMemoryStatusEx(&mut info).expect("GlobalMemoryStatusEx Failed");
-
-        if info.ullTotalPhys <= 2 * 1073741824 {
-            println!("[*] Possibly a virtualised environment")
+        match GlobalMemoryStatusEx(&mut info) {
+            Ok(_) => {
+                let ram_gb = info.ullTotalPhys / (1024 * 1024 * 1024);
+                if ram_gb <= 2 {
+                    println!("Bruh, {}GB RAM? My toaster has more than that!", ram_gb);
+                    1
+                } else {
+                    println!("{}GB RAM. Decent, I guess.", ram_gb);
+                    0
+                }
+            },
+            Err(_) => {
+                println!("Ugh, can't even check the RAM. This computer's being weird.");
+                0
+            }
         }
     }
 }
 
-fn verify_usb() {
+fn check_usb_stuff() -> u32 {
+    println!("Checking out the USB situation...");
     let mut h_key: HKEY = HKEY::default();
-    let mut usb_number: u32 = 0;
+    let mut usb_count: u32 = 0;
     let mut class_name_buffer = [0u8; 256];
     let mut class_name_length = class_name_buffer.len() as u32;
 
     unsafe {
-        let status = RegOpenKeyExA(
+        if RegOpenKeyExA(
             HKEY_LOCAL_MACHINE,
             s!("SYSTEM\\ControlSet001\\Enum\\USBSTOR"),
             0,
             KEY_READ,
             &mut h_key,
-        );
-
-        if status.is_err() {
-            println!("RegOpenKeyExA Failed");
-            return;
+        ).is_ok() {
+            if RegQueryInfoKeyA(
+                h_key,
+                PSTR(class_name_buffer.as_mut_ptr()),
+                Some(&mut class_name_length),
+                None,
+                Some(&mut usb_count),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ).is_ok() {
+                if usb_count < 2 {
+                    println!("Barely any USB stuff. This PC living under a rock or what?");
+                    1
+                } else {
+                    println!("Found {} USB things. That's something, I guess.", usb_count);
+                    0
+                }
+            } else {
+                println!("Ugh, can't even count the USBs. This PC's being difficult.");
+                0
+            }
+            let _ = RegCloseKey(h_key);
+        } else {
+            println!("Can't even look at USB stuff. This computer's got trust issues.");
+            1
         }
-
-        let status = RegQueryInfoKeyA(
-            h_key,
-            PSTR(class_name_buffer.as_mut_ptr()),
-            Some(&mut class_name_length),
-            None,
-            Some(&mut usb_number),
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-        );
-
-        if status.is_err() {
-            println!("RegQueryInfoKeyA Failed");
-            return;
-        }
-
-        if usb_number < 2 {
-            println!("[*] Possibly a virtualised environment");
-            return;
-        }
-
-        let _ = RegCloseKey(h_key);
     }
 }
 
-/*
-  Check's if the instance can be sandboxed through the number of processes running
-  */
-fn verify_processes() {
+fn see_whats_running() -> u32 {
+    println!("Let's see what's running on this thing...");
     let mut system = System::new_all();
     system.refresh_all();
-
-    let number_processes = system.processes().len();
-
-    if number_processes <= 50 {
-        println!("[*] Possibly a sandbox environment");
+    let process_count = system.processes().len();
+    
+    if process_count <= 50 {
+        println!("Only {} things running? Is this PC even awake?", process_count);
+        1
+    } else {
+        println!("Whoa, {} processes! This PC's busier than my group chat!", process_count);
+        0
     }
 }
